0361776942ba0ab0c1112260ea923ef333af794b
Antoine Le Calvez <antoine@alc.io>
2015-08-30 16:24:52 -0400

    fixed infinite recursion

--- bitcoinrpc/authproxy.py
+++ bitcoinrpc/authproxy.py
@@ -74,7 +74,7 @@ class JSONRPCException(Exception):
 
 def EncodeDecimal(o):
     if isinstance(o, decimal.Decimal):
-        return round(o, 8)
+        return round(float(o), 8)
     raise TypeError(repr(o) + " is not JSON serializable")
 
 class AuthServiceProxy(object):
